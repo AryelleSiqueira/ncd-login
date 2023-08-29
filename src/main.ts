9c2f9564763f1d4c0058a5de253ddc8984cc9f62
AryelleSiqueira <aryellesiqueira@hotmail.com>
2023-08-29 20:57:28 -0300

    readme + minor adjustments

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,6 @@ import router from "./router";
 router.beforeEach((to, from, next) => {
     const isAuthenticated: boolean = store.getters.isAuthenticated;
 
-    console.log(isAuthenticated, to.name, typeof isAuthenticated);
-
     if (!isAuthenticated && to.name !== 'LoginView' && to.name !== 'RegisterView') {
         next({ name: 'LoginView' });
     } else if (isAuthenticated && to.name !== 'LoggedView') {
